d59b8d60300d294203ad6664ce7d6af390d66b0f
Boumedienne <cb_bouzidi@esi.dz>
2020-01-19 16:34:55 +0100

    add GET /todos route

--- server/server.js
+++ server/server.js
@@ -8,20 +8,27 @@ var { User } = require('./models/user');
 var app = express();
 
 app.listen(3000, () => {
-    console.log('Started on port 3000');
+  console.log('Started on port 3000');
 });
 app.use(bodyParser.json());
 app.post('/todos', (req, res) => {
-  var todo=new Todo({
-  text:req.body.text
+  var todo = new Todo({
+    text: req.body.text
   });
   todo.save().then((doc) => {
     res.send(doc);
-      console.log('Created sucessufuly');
+    console.log('Created sucessufuly');
 
-}, (e) => {
+  }, (e) => {
     res.status(400).send(e);
+  });
 });
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos})
+  }, (e) => {
+    res.status(400).send(e);
+  })
 });
 
-module.exports = {app};
+module.exports = { app };
